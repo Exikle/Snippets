20129d38662d97a376d67d6ad00f354a8ea007d7
Exikle <dixondcunha@gmail.com>
2014-09-29 21:46:58 -0400

    Added comments and removed useless code

--- src/com/exikle/java/ModNumSolver.java
+++ src/com/exikle/java/ModNumSolver.java
@@ -1,26 +1,34 @@
+package com.exikle.java;
+
 import java.util.Random;
 
+/**
+ * Written to figure out how to get 1-5 as a value from modulos +1
+ * 
+ * @author Dixon D'Cunha
+ * 
+ */
 public class ModNumSolver {
-  static final int TIMES_RUN=20;
-  static int[] counter;
-  public static void main(String[] args) {
-    counter  = new int[5];
-    
-    for(int x=0;x<TIMES_RUN;x++){
-      Random rn = new Random();
-      int i = Math.abs(rn.nextInt() % 5);
-      counter[i]+=1;
-      i+=1;
-      if(i>5)
-        System.out.println("Meh");
-      else
-        System.out.println("Num: " + i);
-    }
-    
-    System.out.println();
-    
-    for(int x=0;x<5;x++){
-      System.out.println((x+1)+" came up: "+counter[x]);
-    }
-  }
+	static final int TIMES_RUN = 20; // The amount of times to fun
+	static int[] counter; // A counter to see how many times a value pops up
+
+	public static void main(String[] args) {
+		counter = new int[5];
+
+		for (int x = 0; x < TIMES_RUN; x++) {
+			int i = Math.abs(new Random().nextInt() % 5);
+			counter[i] += 1; // adds one to the counter
+			i += 1;
+			if (i > 5) { // Just in case the program breaks
+				System.out.println("Meh, I Broke");
+			} else {
+				// System.out.println("Num: " + i); //Pops out the value
+				// generated
+			}
+		}
+
+		for (int x = 0; x < 5; x++) {
+			System.out.println((x + 1) + " came up: " + counter[x]);
+		}
+	}
 }
